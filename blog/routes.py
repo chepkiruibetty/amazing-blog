9eee208a513d890b2d30d5c7300d107281ad2c0c
chepkiruibetty <chepkiruibetty64@gmail.com>
2020-02-15 18:05:28 +0300

    add routing and va;idators for update form

--- blog/routes.py
+++ blog/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, url_for, flash, redirect, request
 from blog import app, db, bcrypt
-from blog.forms import RegistrationForm, LoginForm
+from blog.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from blog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -70,7 +70,17 @@ def logout():
     return redirect(url_for('home'))
 
 
-@app.route("/account")
+@app.route("/account",methods=['GET', 'POST'])
 @login_required
 def account():
-    return render_template('account.html', title='Account')
+    form=UpdateAccountForm()
+    if form.validate_on_submit():
+        current_user.username=form.username.data
+        current_user.email=form.email.data
+        db.session.commit()
+        flash('your account is updated','primary')
+        return redirect(url_for('account'))
+    elif request.method =='GET':
+        form.username.data==current_user.username
+        form.email.data==current_user.email
+    return render_template('account.html', title='Account',form=form)
